945d7fdcb4774a9e209575ea10542351a1640413
shah <shahjehan737@gmail.com>
2023-07-31 09:32:37 -0700

    s

--- move.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ls helm/profilecharts/templates/ 2>/dev/null
-if [ $? -eq 0 ]; then
-  rm -rf helm/profilecharts/templates/*
-  cp *.yaml helm/profilecharts/templates/
-else
-  mkdir helm/profilecharts/templates/
-  cp *.yaml helm/profilecharts/templates/
-fi
