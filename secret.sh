e82e9bde3c4d2ac7173c9343c6e563cee92a6112
shah <shahjehan737@gmail.com>
2023-07-31 02:46:41 -0700

    helm configured

---
+++ secret.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Store the base64-encoded contents of config.json in a variable
+SECRET_DATA=$(base64 -w 0 config.json)
+
+rm secret.yaml
+
+CERT_BASE64=$(base64 -w 0 < cert)
+KEY_BASE64=$(base64 -w 0 < key)
+
+# Create a Kubernetes Secret YAML file with the variable as the data value
+cat <<EOF > secret.yaml
+apiVersion: v1
+kind: Secret
+metadata:
+  name: secret-tiger-docker
+  namespace: profile-site
+  type: kubernetes.io/dockerconfigjson
+data:
+  .dockerconfigjson: $SECRET_DATA
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: wildcard-certificate
+  namespace: profile-site
+type: kubernetes.io/tls
+data:
+  tls.crt: $CERT_BASE64
+  tls.key: $KEY_BASE64
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: wildcard-certificate
+  namespace: ingress-nginx
+type: kubernetes.io/tls
+data:
+  tls.crt: $CERT_BASE64
+  tls.key: $KEY_BASE64
+EOF
+
+rm -rf helm/profilecharts/templates/*
+
+cp *.yaml helm/profilecharts/templates/
+cp cert helm/profilecharts/templates/
+cp key helm/profilecharts/templates/
--- start-kubeclient.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-kops update cluster --name kubecluster.shahdevelopment.tech --state=s3://kubedevops001 --yes --admin
