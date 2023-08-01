882d5f9af04732576710ea9019015a160c637049
shah <shahjehan737@gmail.com>
2023-08-01 07:13:18 -0700

    backend chanfe

--- move.sh
+++ move.sh
@@ -7,3 +7,12 @@ else
   mkdir helm/profilecharts/templates/
   cp *.yaml helm/profilecharts/templates/
 fi
+
+# ls ~/.kube/  2>/dev/null
+# if [ $? -eq 0 ]; then
+#   cp config ~/.kube/config
+# else
+#   mkdir ~/.kube/
+#   cp config ~/.kube/config
+# fi
+
